9d689a39c2c08ca542bd506a05e88cdfc4662ec5
lewiskirori <amicableycot@gmail.com>
2024-01-22 07:16:31 +0300

    context spec variations

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -2,14 +2,36 @@
 
 import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
+import Input from "./Input";
 
 const UploadModal = () => {
+    const [isLoading, setIsLoading] = useState();
     const uploadModal = useUploadModal();
+
+    const {
+        register,
+        handleSubmit,
+        reset
+    } = useForm<FieldValues>({
+        defaultValues: {
+            author: '',
+            title: '',
+            song: null,
+            image: null,
+        }
+    })
 
     const onChange = (open: boolean) => {
         if (!open) {
+            reset();
             uploadModal.onClose();
         }
+    }
+
+    const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+
     }
 
     return (
@@ -20,7 +42,47 @@ const UploadModal = () => {
             onChange={onChange}
 
         >
-            Content
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col gap-y-4"
+            >
+                <Input 
+                    id="title"
+                    disabled={isLoading}
+                    {...register('title', { required: true })}
+                    placeholder="Song Name"
+                />
+                <Input 
+                    id="author"
+                    disabled={isLoading}
+                    {...register('author', { required: true })}
+                    placeholder="Artist Name"
+                />
+                <div>
+                    <div className="pb-1">
+                        Pick a song file
+                    </div>
+                    <Input 
+                        id="song"
+                        type="file"
+                        disabled={isLoading}
+                        accept=".m4a, .flac, .mp3, .wav, .wma, .aac"
+                        {...register('song', { required: true })}
+                    />
+                </div>
+                <div>
+                    <div className="pb-1">
+                        Select a picture
+                    </div>
+                    <Input 
+                        id="image"
+                        type="file"
+                        disabled={isLoading}
+                        accept="image/*"
+                        {...register('image', { required: true })}
+                    />
+                </div>
+            </form>
         </Modal>
     );
 }
