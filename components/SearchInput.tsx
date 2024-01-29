3d390fd2942c93377bac55f93277445984171b3a
lewiskirori <amicableycot@gmail.com>
2024-01-29 11:39:25 +0300

    design preferences

---
+++ components/SearchInput.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import qs from "query-string";
+import Input from "./Input";
+
+const SearchInput = () => {
+    const router = useRouter();
+    const [value, setValue] = useState<string>("");
+    const debouncedValue = useDebounce<string>(value, 500);
+
+    useEffect(() => {
+        const query = {
+            title: debouncedValue,
+        };
+
+        const url = qs.stringifyUrl({
+            url: '/search',
+            query: query
+        });
+
+        router.push(url);
+    }, [debouncedValue, router]);
+
+    return (
+        <Input
+            className="border-2 border-transparent rounded-full px-4 py-3 text-lg focus:outline-none hover:border-gray-300 focus:border-white focus:border-2"
+            placeholder="What are you in the mood for?"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+        />
+    );
+}
+
+export default SearchInput;
