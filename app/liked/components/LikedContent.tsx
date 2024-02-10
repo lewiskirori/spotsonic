4cbac47bf0f772af084c0936b16aaa6909a22b82
lewiskirori <amicableycot@gmail.com>
2024-02-10 15:37:17 +0300

    configuration Patterns

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import LikeButton from "@/components/LikeButton";
+import MediaItem from "@/components/MediaItem";
 import { useUser } from "@/hooks/useUser";
 import { Song } from "@/types";
 import { useRouter } from "next/navigation";
@@ -38,7 +40,20 @@ const LikedContent: React.FC<LikedContentProps> = ({
 
     return (
         <div className="flex flex-col gap-y-2 w-full p-6">
-            Liked Content..
+            {songs.map((song) => (
+                <div
+                    key={song.id}
+                    className="flex items-center gap-x-4 w-full"
+                >
+                    <div className="flex-1">
+                        <MediaItem 
+                            onClick={() => {}}
+                            data={song}
+                        />
+                    </div>
+                    <LikeButton songId={song.id} />
+                </div>
+            ))}
         </div>
     );
 }
