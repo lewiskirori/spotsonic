f9102e66019a4bc2fa520659a90685242071a8e5
lewiskirori <amicableycot@gmail.com>
2024-02-15 20:51:46 +0300

    access operation

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -8,6 +8,8 @@ import { ImPlay3 } from "react-icons/im";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 import Slider from "./Slider";
+import usePlayer from "@/hooks/usePlayer";
+import { useState } from "react";
 
 interface PlayerContentProps {
     song: Song;
@@ -18,8 +20,44 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     song,
     songUrl
 }) => {
-    const Icon = true ? MdPause : ImPlay3;
-    const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+    const player = usePlayer();
+    const [volume, setVolume] = useState(1);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const Icon = isPlaying ? MdPause : ImPlay3;
+    const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+    const onPlayNext = () => {
+        if (player.ids.length === 0) {
+            return;
+        }
+
+        const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+        const nextSong = player.ids[currentIndex + 1];
+
+        if (!nextSong) {
+            return player.setId(player.ids[0]);
+        }
+
+        player.setId(nextSong);
+    };
+
+    const onPlayPrev = () => {
+        if (player.ids.length === 0) {
+            return;
+        }
+
+        const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+        const prevSong = player.ids[currentIndex - 1];
+
+        if (!prevSong) {
+            return player.setId(player.ids[player.ids.length - 1]);
+        }
+
+        player.setId(prevSong);
+    };
+
+    
 
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -73,7 +111,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                 "
             >
                 <AiFillStepBackward
-                    onClick={() => {}} 
+                    onClick={onPlayPrev} 
                     size={30}
                     className="
                         text-neutral-400
@@ -99,7 +137,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                     <Icon size={30} className="text-black" />
                 </div>
                 <AiFillStepForward
-                    onClick={() => {}}
+                    onClick={onPlayNext}
                     size={30}
                     className="
                         text-neutral-400
@@ -115,7 +153,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                     <VolumeIcon 
                         onClick={() => {}}
                         className="text-neutral-400 cursor-pointer hover:text-white transition"
-                        size={30}
+                        size={34}
                     />
                     <Slider />
                 </div>
