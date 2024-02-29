e5141b90b36ca92a9ea70f04a32233ad7b31b86a
lewiskirori <amicableycot@gmail.com>
2024-02-29 23:33:36 +0300

    class elements

---
+++ app/search/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Box from "@/components/Box";
+
+const Error = () => {
+    return (
+        <Box className="h-full flex items-center justify-center">
+            <div className="text-neutral-400 flex flex-col items-center">
+                <span style={{ fontSize: '1.2rem' }}>
+                    Oh sorry — it looks like something went wrong!
+                </span>
+                <br />
+                <span style={{ fontSize: '0.9rem', marginTop: '0.5rem' }}>
+                    Try reloading.
+                </span>
+            </div>
+        </Box>
+    );
+};
+
+export default Error;
